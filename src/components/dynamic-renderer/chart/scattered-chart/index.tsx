297dbd6b4ba8c795630df11db5afa8a6ead417fd
manu-hn <manuu.hn11@gmail.com>
2025-11-27 10:59:09 +0530

    `Updated title in ScatteredChart component`

--- src/components/dynamic-renderer/chart/scattered-chart/index.tsx
+++ src/components/dynamic-renderer/chart/scattered-chart/index.tsx
@@ -18,7 +18,7 @@ function ScatteredChart({chartData, title} : {chartData: any, title : string}) {
 
   return (
     <section className="w-full border-b">
-      <h3 className={`text-[1.05rem] font-bold`}>Soil 0{title}</h3>
+      <h3 className={`text-[1.05rem] font-bold`}>Soil {title}</h3>
       <div className="w-full">
         <Plot data={chartData} layout={layout} />
       </div>
